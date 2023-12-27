070f50214b08804f5b50d2556432de8343b2ccfd
suchanon456 <137870206+suchanon456@users.noreply.github.com>
2023-12-27 15:50:10 +0700

    StartegyProjectScreen.php

--- app/Orchid/Screens/Startegy/StartegyProjectScreen.php
+++ app/Orchid/Screens/Startegy/StartegyProjectScreen.php
@@ -2,10 +2,36 @@
 
 namespace App\Orchid\Screens\Startegy;
 
+use App\Models\Area;
+use App\Models\InspectionArea;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Fields\Group;
+use Orchid\Screen\Fields\Relation;
 use Orchid\Screen\Screen;
+use Orchid\Support\Facades\Layout;
+use Orchid\Support\Color;
+use Orchid\Screen\Fields\Label;
+use Orchid\Support\Facades\Toast;
 
 class StartegyProjectScreen extends Screen
 {
+    public $areaData;
+    public $inspection_id;
+    public $areas;
+    public $template_project;
+    public $template_rows;
+    public $template_cols;
+    
+    public function __construct()
+    {
+        $this->template_cols1        = ["code" => "0000-00-000", "project_name"  => "1.โครงการที่หนึ่งลดภาระครู"];
+        $this->template_cols2        = ["code" => "0000-00-001", "project_name"  => "1.โครงการที่หนึ่งลดภาระครู"];
+        $this->template_rows        = ["cols1" => [$this->template_cols1], "cols2"  => [$this->template_cols2]];
+        $this->template_project        = ["rows"  => [$this->template_rows]];
+    }
     /**
      * Fetch data to be displayed on the screen.
      *
@@ -13,7 +39,13 @@ class StartegyProjectScreen extends Screen
      */
     public function query(): iterable
     {
-        return [];
+        return [
+            'inspections' => InspectionArea::all(),
+            'areaData' => $this->areaData ?? Auth::user()->area,
+            'areas' => $this->areas ?? Auth::user()->area->byInspection(Auth::user()->area->inspection_id)->select(['id', 'name', 'inspection_id'])->get(),
+            'inspection_id'  => $this->inspection_id ?? Auth::user()->area->inspection_id,
+            'goals' => $this->goals(),
+        ];
     }
     /**
      * Permission
@@ -35,7 +67,7 @@ class StartegyProjectScreen extends Screen
      */
     public function name(): ?string
     {
-        return 'StartegyProjectScreen';
+        return 'แผนงาน / โครงการ';
     }
 
     /**
@@ -47,6 +79,14 @@ class StartegyProjectScreen extends Screen
     {
         return [];
     }
+    
+    /**
+     * @return string|null
+     */
+    public function description(): ?string
+    {
+        return $this->areaData->name ?? Auth::user()->area->name;
+    }
 
     /**
      * The screen's layout elements.
@@ -55,6 +95,32 @@ class StartegyProjectScreen extends Screen
      */
     public function layout(): iterable
     {
-        return [];
+        return [
+            Layout::view('Forms.contex_output'),
+        ];
+    }
+    
+    public function getArea(Request $request)
+    {
+        #todo bug when change inspection_id
+        $area_id = $request->area_id;
+        $inspection_id = $this->inspection_id;
+        if ($inspection_id != $request->inspection_id) {
+            // dd($request->inspection_id);
+            $this->areas = Area::byInspection($request->inspection_id)->select(['id', 'name', 'inspection_id'])->get();
+            $this->inspection_id = $request->inspection_id;
+            $area_id = $this->areas[0]->id;
+        }
+        $this->areaData = Area::where('id', $area_id)->where('inspection_id', $request->inspection_id)->first();
+    }
+    
+    
+    function push_goal()
+    {
+        return back();
+    }
+    public function goals(): array
+    {
+        return $goals = [$this->template_project];
     }
 }
