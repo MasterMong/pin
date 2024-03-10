5e349cd303c0093d0e1c59539bf4ea021d2f6783
MasterMong <mongkon@outlook.com>
2024-03-10 20:38:05 +0700

    fix show empty innovation

--- resources/views/tables/columns/column-innovation-list.blade.php
+++ resources/views/tables/columns/column-innovation-list.blade.php
@@ -1,9 +1,11 @@
 <div>
     <ul class="list-disc pl-3">
-        @foreach($getState() as $innovation)
-            <li>
-                {{$innovation->name}}
-            </li>
-        @endforeach
+        @if(count($getState()) > 0)
+            @foreach($getState() as $innovation)
+                <li>
+                    {{$innovation->name}}
+                </li>
+            @endforeach
+        @endif
     </ul>
 </div>
