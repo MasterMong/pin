02639cb0667a742af87de5d5d8ea09d6ad9430c1
MasterMong <mongkon@outlook.com>
2024-03-18 11:02:34 +0700

    update area

--- app/Filament/Pages/AreaInfo.php
+++ app/Filament/Pages/AreaInfo.php
@@ -2,6 +2,10 @@
 
 namespace App\Filament\Pages;
 
+use App\Models\Area;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Infolists\Components\Section;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Contracts\HasInfolists;
@@ -22,6 +26,85 @@ class AreaInfo extends Page implements HasInfolists
     public static function getNavigationLabel(): string
     {
         return auth()->user()->area->areaType->name . ' ' . auth()->user()->area->name;
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            EditAction::make()
+                ->record(Area::where('id', auth()->user()->area_id)->first())
+                ->modelLabel('ข้อมูล สพท.')
+                ->form([
+                    TextInput::make('name')
+                        ->label('ชื่อ สพท.')
+                        ->required()
+                        ->maxLength(300),
+                    Select::make('inspection_area_id')
+                        ->label('เขตตรวจราชการ')
+                        ->relationship('inspectionArea', 'name')
+                        ->required(),
+                    Select::make('area_type_id')
+                        ->label('ประเภท')
+                        ->relationship('areaType', 'name')
+                        ->required(),
+                    \Filament\Forms\Components\Section::make()->label('ข้อมูลติดต่อ')->schema([
+                        Select::make('district_id')
+                            ->label('จังหวัด')
+                            ->relationship('district', 'name_in_thai')
+                            ->required(),
+                        Select::make('province_id')
+                            ->label('อำเภอ')
+                            ->relationship('province', 'name_in_thai')
+                            ->required(),
+                        Select::make('region_id')
+                            ->label('ภูมิภาค')
+                            ->relationship('region', 'name')
+                            ->required(),
+                        TextInput::make('address')
+                            ->label('ที่อยู่')
+                            ->maxLength(600),
+                        TextInput::make('zip_code')
+                            ->label('รหัสไปรษณีย์')
+                            ->maxLength(5),
+                        TextInput::make('tel')
+                            ->label('โทรศัพท์')
+                            ->tel()
+                            ->required()
+                            ->maxLength(20),
+                    ])->columns(2),
+                    TextInput::make('num_person')
+                        ->label('จำนวนบุคลากร')
+                        ->required()
+                        ->numeric()
+                        ->default(0),
+                    TextInput::make('num_school')
+                        ->label('จำนวนโรงเรียน')
+                        ->required()
+                        ->numeric()
+                        ->default(0),
+                    TextInput::make('num_teacher')
+                        ->label('จำนวนครู')
+                        ->required()
+                        ->numeric()
+                        ->default(0),
+                    TextInput::make('num_student')
+                        ->label('จำนวนนักเรียน')
+                        ->required()
+                        ->numeric()
+                        ->default(0),
+                    TextInput::make('website')
+                        ->label('เว็บไซต์')
+                        ->required()
+                        ->maxLength(600),
+                    TextInput::make('latitude')
+                        ->required()
+                        ->maxLength(30),
+                    TextInput::make('longitude')
+                        ->required()
+                        ->maxLength(30)
+                ])
+                ->slideOver()
+        ];
     }
 
     public function getTitle(): string
@@ -51,4 +134,5 @@ class AreaInfo extends Page implements HasInfolists
                 ])
             ]);
     }
+
 }
