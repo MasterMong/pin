9e50c7178d077f9fbf82a7d9c2ce2d1771ef174a
MasterMong <mongkon@outlook.com>
2024-03-18 09:10:07 +0700

    update dashboard link

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-filament-panels::page>
     <div>
         <iframe class="rounded" width="100%" height="800px"
-                src="https://app.powerbi.com/reportEmbed?reportId=0d764881-e9a5-4cf2-b6aa-352f76ff8662&autoAuth=true&ctid=99f7a6e8-d7f5-4524-bc27-d1ee50bc2d4b"
+                src="https://app.powerbi.com/reportEmbed?reportId=66dfd273-aea7-48d9-9f65-1295411ac05e&autoAuth=true&ctid=99f7a6e8-d7f5-4524-bc27-d1ee50bc2d4b"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowfullscreen>
